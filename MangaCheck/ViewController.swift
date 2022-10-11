076be1fb51c40dbcca245546184d1ae7786b60b8
SarahMadalena <sarahmadalena68@gmail.com>
2022-10-11 11:29:43 -0300

    Initial Commit

---
+++ MangaCheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MangaCheck
+//
+//  Created by Sarah Madalena on 11/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
